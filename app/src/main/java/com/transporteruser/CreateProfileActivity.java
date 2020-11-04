58e9570640afcd342a033d1c2cd84da1964acaa2
Vaishnavi8371 <vaishnavi8371@gmail.com>
2020-11-04 17:29:37 +0530

    Login ui and code and create profile ui

---
+++ app/src/main/java/com/transporteruser/CreateProfileActivity.java
@@ -0,0 +1,16 @@
+package com.transporteruser;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_profile);
+    }
+
+}
