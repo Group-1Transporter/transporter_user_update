4bf4f1160b6dffbb3e0957809436ae070b081369
ruhil_hayat <githubdevelopertransport@gmail.com>
2020-12-01 18:10:12 +0530

    code notification

--- app/src/main/java/com/transporteruser/adapters/CreatedLeadShowAdapter.java
+++ app/src/main/java/com/transporteruser/adapters/CreatedLeadShowAdapter.java
@@ -1,18 +1,30 @@
 package com.transporteruser.adapters;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.transporteruser.BidShowActivity;
+import com.transporteruser.CreateProfileActivity;
+import com.transporteruser.api.UserService;
 import com.transporteruser.bean.Lead;
 import com.transporteruser.databinding.CreatedLoadBinding;
 
 import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class CreatedLeadShowAdapter extends RecyclerView.Adapter<CreatedLeadShowAdapter.CreatedViewHolder> {
     OnRecyclerViewClickListner listner;
@@ -32,8 +44,8 @@ public class CreatedLeadShowAdapter extends RecyclerView.Adapter<CreatedLeadShow
     }
 
     @Override
-    public void onBindViewHolder(@NonNull final CreatedViewHolder holder, int position) {
-        Lead lead = leadList.get(position);
+    public void onBindViewHolder(@NonNull final CreatedViewHolder holder, final int position) {
+        final Lead lead = leadList.get(position);
         String str[]=lead.getPickUpAddress().split(" ");
         String pickup=str[str.length-1];
         str=lead.getDeliveryAddress().split(" ");
@@ -43,6 +55,62 @@ public class CreatedLeadShowAdapter extends RecyclerView.Adapter<CreatedLeadShow
         holder.binding.tvCLPickUpContact.setText(lead.getContactForPickup());
         holder.binding.tvCLDeliveryContact.setText(lead.getContactForDelivery());
         holder.binding.tvQuntity.setText(lead.getWeight());
+        if (lead.getBidCount() !=null){
+            holder.binding.llCounter.setVisibility(View.VISIBLE);
+            holder.binding.tvCounter.setText("" + lead.getBidCount());
+        }
+        else {
+            holder.binding.llCounter.setVisibility(View.GONE);
+        }
+        holder.binding.morevertical.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popupMenu = new PopupMenu(holder.itemView.getContext(),holder.binding.morevertical);
+                final Menu menu= popupMenu.getMenu();
+                menu.add("Delete");
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast.makeText(holder.itemView.getContext(), "Delete", Toast.LENGTH_SHORT).show();
+                        String title = menuItem.getTitle().toString();
+                        if (title.equalsIgnoreCase("Delete")){
+                            final UserService.UserApi userApi = UserService.getUserApiInstance();
+                            AlertDialog.Builder ab = new AlertDialog.Builder(holder.itemView.getContext());
+                            ab.setTitle("DELETE");
+                            ab.setMessage("Are You Sure ?");
+                            ab.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    userApi.deleteLeadById(lead.getLeadId()).enqueue(new Callback<ArrayList<Lead>>() {
+                                        @Override
+                                        public void onResponse(Call<ArrayList<Lead>> call, Response<ArrayList<Lead>> response) {
+                                            if(response.code() == 200){
+                                                leadList.remove(position);
+                                                notifyDataSetChanged();
+                                                Toast.makeText(holder.itemView.getContext(), "Lead Deleted", Toast.LENGTH_SHORT).show();
+                                            }
+                                            else
+                                                Toast.makeText(holder.itemView.getContext(), "Something Went Wrong", Toast.LENGTH_SHORT).show();
+                                        }
+
+                                        @Override
+                                        public void onFailure(Call<ArrayList<Lead>> call, Throwable t) {
+                                            Toast.makeText(holder.itemView.getContext(), ""+t.getMessage(), Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                                }
+                            });
+                            ab.setNegativeButton("No",null);
+                            ab.show();
+                        }
+                        return false;
+
+                    }
+                });
+                popupMenu.show();
+            }
+
+        });
     }
 
     @Override
@@ -67,6 +135,7 @@ public class CreatedLeadShowAdapter extends RecyclerView.Adapter<CreatedLeadShow
            });
        }
    }
+
 
 
    public interface OnRecyclerViewClickListner{
--- app/src/main/java/com/transporteruser/api/ServerAddress.java
+++ app/src/main/java/com/transporteruser/api/ServerAddress.java
@@ -1,6 +1,6 @@
 package com.transporteruser.api;
 
 public interface ServerAddress {
-  String BASE_URL = "http://192.168.0.102:8080";
+  String BASE_URL = "http://192.168.0.105:8080";
 
 }
